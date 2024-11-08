c40ee838938daa4452fcee62ab918dbb9db6ad3b
Alejo <alejojpa2@gmail.com>
2024-11-08 07:19:19 -0500

    friends.js was updated

--- index.js
+++ index.js
@@ -107,73 +107,7 @@ app.post("/register", (req, res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
-//API endpoint to retrieve friendly users (Authenticated users)
-// route handler for GET requests to the root path "/"
-router.get("/",(req, res) => {
-    res.send(JSON.stringify(friends, null, 4));
-});
 
-//API endpoint to retrieve Authenticated users email and send it to the client
-router.get("/:email", (req,res) =>{
-    //Retrieve email parameter and send it to client
-    const email = req.params.email;
-    res.send(friends[email]);
-});
-
-//Add (CRUD: CREATE, post) the new user to the JSON/dictionary (i.e. friends = {...} @ router/friends.js)
-router.post("/", (req,res)=>{
-    // Check if email is provided in the request body
-    if (req.body.email) {
-        friends[req.body.email]= {
-            "firstName" : req.body.firstName,
-            "lastName" : req.body.lastName,
-            "DOB": req.body.BOD
-        };
-    }
-    res.send("The user" + (' ') + (req.body.firstName) + " Has been added!");
-});
-
-//Update (CRUD: UPDATE, put) a user in the JSON/dictionary (i.e. friends = {...} @ router/friends.js)
-router.put("/email", (req,res)=>{
-    const email = req.params.email;
-    let friend = friends[email]; // Retrieve friend object associated with email
-    if (friend) {
-        let DOB = req.body.DOB;
-        let firstName = req.body.firstName;
-        let lastName = req.body.lastName;
-
-        // Update DOB,firstName and lastName if provided in request body
-        if (DOB) {
-            friend["DOB"] = DOB;
-        };
-
-        if (firstName) {
-            friend["firstName"] = firstName;
-        };
-
-        if (lastName) {
-            friend["lastName"] = lastName;
-        };
-
-        friends[email] = friend; // Update friend details in 'friends' object
-        res.send(`Friend with the email ${email} updated.`);
-    } else {
-        res.send(`Unable to find friend!`);
-    }
-});
-
-//Delete friend
-router.delete("/:email", (req, res) => {
-    // Extract email parameter from request URL
-    const email = req.params.email;
-    
-    if (email) {
-        // Delete friend from 'friends' object based on provided email
-        delete friends[email];
-    }
-    // Send response confirming deletion of friend
-    res.send(`Friend with the email ${email} deleted.`);
-});
 
 
 const PORT =5000;
